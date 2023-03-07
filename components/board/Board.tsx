469b041dab92d8b2d93995f8216babd6fda55d93
dldvk9999 <47880131+dldvk9999@users.noreply.github.com>
2023-03-07 21:46:35 +0900

    Delete components/board directory

--- components/board/Board.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Board.module.scss";
-
-export default function Board() {
-    return (
-        <main>
-            <section>This is Board page.</section>
-        </main>
-    );
-}
