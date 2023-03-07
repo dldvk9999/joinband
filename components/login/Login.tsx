0389707017e61e547c1f2d6ebce9ff6800fb7a71
dldvk9999 <47880131+dldvk9999@users.noreply.github.com>
2023-03-07 21:46:53 +0900

    Delete components/login directory

--- components/login/Login.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Login.module.scss";
-
-export default function Login(props: { type: "login" | "signup" }) {
-    return (
-        <main>
-            <section>This is Login{"(" + props.type + ")"} page.</section>
-        </main>
-    );
-}
