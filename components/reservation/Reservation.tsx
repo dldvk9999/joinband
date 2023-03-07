be21828a5c35240dfcf40433355f673c011b37ce
dldvk9999 <47880131+dldvk9999@users.noreply.github.com>
2023-03-07 21:47:05 +0900

    Delete components/reservation directory

--- components/reservation/Reservation.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Reservation.module.scss";
-
-export default function Reservation() {
-    return (
-        <main>
-            <section>This is Reservation page.</section>
-        </main>
-    );
-}
