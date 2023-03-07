0d7ca429992a84dc6b881f932375f3f933678a69
dldvk9999 <47880131+dldvk9999@users.noreply.github.com>
2023-03-07 21:46:45 +0900

    Delete components/invite directory

--- components/invite/Invite.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Invite.module.scss";
-
-export default function Invite() {
-    return (
-        <main>
-            <section>This is Invite page.</section>
-        </main>
-    );
-}
