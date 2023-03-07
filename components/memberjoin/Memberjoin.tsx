0e08abc1328252a12ee277dae51b46ea10eb5618
dldvk9999 <47880131+dldvk9999@users.noreply.github.com>
2023-03-07 21:47:00 +0900

    Delete components/memberjoin directory

--- components/memberjoin/Memberjoin.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Memberjoin.module.scss";
-
-export default function Memberjoin() {
-    return (
-        <main>
-            <section>This is Memberjoin page.</section>
-        </main>
-    );
-}
