b8787869363092aed5aee3b0807524795ca94658
JongGeun <dldvk9999@naver.com>
2023-03-18 22:34:50 +0900

    Mypage - Redux Router setting

--- components/Mypage/Mypage.tsx
+++ components/Mypage/Mypage.tsx
@@ -2,6 +2,8 @@ import styles from "./Mypage.module.scss";
 import Nav from "./Nav/Nav";
 import Information from "./Information/Information";
 import Video from "./Video/Video";
+import ChangePassword from "./ChangePassword/ChangePassword";
+import DeleteUser from "./DeleteUser/DeleteUser";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store/configureStore";
 
@@ -10,8 +12,8 @@ export default function Mypage() {
 
     function pageView() {
         if (counterValue === 0) return <Information />;
-        else if (counterValue === 2) return <Information />;
-        else if (counterValue === 3) return <Information />;
+        else if (counterValue === 2) return <ChangePassword />;
+        else if (counterValue === 3) return <DeleteUser />;
         else return <Video />;
     }
 
