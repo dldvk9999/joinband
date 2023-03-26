c74ba1207309ff0d30252669d1f51683e0263479
JongGeun <dldvk9999@naver.com>
2023-03-26 22:11:45 +0900

    Mypage - create mock data

---
+++ data/MypageData.tsx
@@ -0,0 +1,69 @@
+export const data = {
+    id: 1,
+    name: "Admin",
+    email: "dldvk9999@naver.com",
+    password: "qweqweqwe",
+    profile: "",
+    introduce: "좋았다면 추억이고 나빴다면 경험이다.",
+    nickname: "IamAdmin",
+    part: "일렉기타",
+    live: "인천시 OO구",
+    video: [
+        {
+            id: 1,
+            link: "https://www.youtube.com/watch?v=CTtUeNabfDk",
+        },
+        {
+            id: 2,
+            link: "https://www.youtube.com/watch?v=zm1PefdbJcg",
+        },
+        {
+            id: 3,
+            link: "https://www.youtube.com/watch?v=9hbsPQff2_U",
+        },
+        {
+            id: 4,
+            link: "https://www.youtube.com/watch?v=DLi7YQ-khe8",
+        },
+        {
+            id: 5,
+            link: "https://www.youtube.com/watch?v=umAj7mk1wxQ",
+        },
+        {
+            id: 6,
+            link: "https://www.youtube.com/watch?v=DN72_RXD9r4",
+        },
+        {
+            id: 7,
+            link: "https://www.youtube.com/watch?v=sDF5-W6Q8dE",
+        },
+        {
+            id: 8,
+            link: "https://www.youtube.com/watch?v=Iff1r3pl3yg",
+        },
+    ],
+    group: {
+        id: 1,
+        name: "관리자의 그룹",
+        user: [
+            {
+                id: 1,
+                name: "Admin",
+                email: "dldvk9999@naver.com",
+                part: "일렉기타",
+            },
+            {
+                id: 2,
+                name: "Admin2",
+                email: "qweqwe@naver.com",
+                part: "통기타",
+            },
+            {
+                id: 3,
+                name: "Admin3",
+                email: "qweqweqwe@naver.com",
+                part: "드럼",
+            },
+        ],
+    },
+};
