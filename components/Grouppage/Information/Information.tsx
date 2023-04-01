1d430f506d0127a1968d97135289cbf91ab3b5e1
JongGeun <dldvk9999@naver.com>
2023-04-01 23:13:45 +0900

    Grouppage - Information delete user update

--- components/Grouppage/Information/Information.tsx
+++ components/Grouppage/Information/Information.tsx
@@ -12,10 +12,22 @@ interface usersType {
 export default function Information(props: { id: number }) {
     const [users, setUsers] = useState<Array<usersType>>([]);
 
+    // 그룹 유저 삭제
+    function delUser(index: number, id: number, name: string) {
+        if (index !== 0) {
+            if (confirm(name + " 유저를 삭제하시겠습니까?")) {
+                setUsers(users.filter((item) => item.id !== id));
+                alert("삭제 완료되었습니다.");
+            }
+        }
+    }
+
     // 그룹 초대 링크 보내기
     function invite() {
         const email = prompt("초대링크를 보낼 이메일을 입력해주세요.", "example@naver.com");
-        alert(email + " 전송 완료!");
+        if (email) {
+            alert(email + " 전송 완료!");
+        }
     }
 
     useEffect(() => {
@@ -31,12 +43,14 @@ export default function Information(props: { id: number }) {
             <hr />
             <div className={styles.informationList}>
                 <div>
+                    <p>* 유저를 클릭하여 삭제할 수 있습니다.</p>
                     <button onClick={() => invite()}>초대</button>
                 </div>
                 {users.map((item, i) => {
                     return (
                         <div
-                            className={`${styles.informationListUser} ${styles.informationListUserFirst}`}
+                            className={`${styles.informationListUser} ${i === 0 && styles.informationListUserFirst}`}
+                            onClick={() => delUser(i, item.id, item.name)}
                             key={"grouppage-info-" + i}
                         >
                             <p>{item.name}</p>
