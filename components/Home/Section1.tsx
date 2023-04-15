1688a0f7eefddb459d72412e3c40fd64403c33ac
JongGeun <dldvk9999@naver.com>
2023-04-15 14:54:14 +0900

    Home - PWA Install button update

--- components/Home/Section1.tsx
+++ components/Home/Section1.tsx
@@ -2,8 +2,31 @@ import styles from "./Home.module.scss";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Section1() {
+    const [deferredPrompt, setDeferredPrompt] = useState<any>();
+
+    const handleInstall = () => {
+        if (deferredPrompt) {
+            deferredPrompt.prompt();
+
+            deferredPrompt.userChoice.then((choiceResult: { outcome: string }) => {
+                if (choiceResult.outcome === "accepted") {
+                    // console.log("사용자가 앱 설치를 동의했습니다.");
+                } else {
+                    // console.log("사용자가 앱 설치를 동의하지 않았습니다.");
+                }
+                setDeferredPrompt(null);
+            });
+        }
+    };
+
+    const handleBeforeInstallPrompt = (e: Event) => {
+        e.preventDefault();
+        setDeferredPrompt(e);
+    };
+
     // 홈 화면 캐러셀 이미지 출력
     function carousel() {
         let result = [];
@@ -24,6 +47,14 @@ export default function Section1() {
         return result;
     }
 
+    useEffect(() => {
+        window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+
+        return () => {
+            window.removeEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+        };
+    }, []);
+
     return (
         <section className={styles.homeCarousel}>
             <div className={styles.homeCarouselFront}>
@@ -31,6 +62,11 @@ export default function Section1() {
                 <div className={styles.homeCarouselTitle}>
                     <p>좋은 밴드, 좋은 합주, 좋은 공연</p>
                     <h1>JOINBAND</h1>
+                    {deferredPrompt && (
+                        <i className={styles.homeInstall} onClick={handleInstall}>
+                            Install
+                        </i>
+                    )}
                 </div>
             </div>
 
