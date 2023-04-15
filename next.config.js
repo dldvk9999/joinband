aaccbc1714c2da103a8804156e47645b7768cb14
JongGeun <dldvk9999@naver.com>
2023-04-15 14:07:04 +0900

    PWA - init setting

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 import path from "path";
 import { fileURLToPath } from "url";
+import nextPWA from "next-pwa";
+import runtimeCaching from "next-pwa/cache.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const nextConfig = {
+const config = {
     reactStrictMode: false,
     swcMinify: true,
     // async rewrites() {
@@ -24,5 +26,12 @@ const nextConfig = {
         imageSizes: [200],
     },
 };
+
+const nextConfig = nextPWA({
+    dest: "public",
+    // disable: !isProduction,
+    runtimeCaching: [],
+    runtimeCaching: runtimeCaching,
+})(config);
 
 export default nextConfig;
